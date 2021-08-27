c5964a3d55ab412f624190ff834acfd4a88375be
T-PON1178 <diegosocool456@gmail.com>
2021-08-26 17:47:03 -0700

    added cockgenerator

---
+++ cockgenerator.js
@@ -0,0 +1,8 @@
+function generate_cock(cock_type) {
+    var new_cock = document.createElement("IMG")
+    new_cock.setAttribute("src", "./images/shlong.png") 
+    new_cock.setAttribute("width", "500")  
+    new_cock.setAttribute("height", "333") 
+    new_cock.setAttribute("class", "image")
+    print()
+}
